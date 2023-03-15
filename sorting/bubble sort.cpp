a047060e9bd3190b7ac3f62b77f8efc42b69650d
Raghav Bang <raghavbang2012@gmail.com>
2023-03-15 02:40:46 +0000

    code

--- sorting/bubble sort.cpp
+++ sorting/bubble sort.cpp
@@ -3,15 +3,27 @@
 
 using namespace std;
 
+// We use bubble sort when in Q we see, bring ith element on its correct position.
+// Also sinking sort is its another name.
+// Bubble sort is stable sort
+// In Bubble sort we are pushing ith largest element to right side.
+
 void bubbleSort(vector<int> arr) {
 
   for (int rounds = 1; rounds < arr.size(); rounds++) {
 
+    // Best Case TC O(N), worst case O(N^2)
+    bool swapped = false;
     for (int i = 0; i < arr.size() - rounds; i++) {
-
+ 
       if (arr[i] > arr[i + 1]) {
+        swapped = true;
         swap(arr[i], arr[i + 1]);
       }
+    }
+
+    if (!swapped) {
+      break;
     }
   }
 
