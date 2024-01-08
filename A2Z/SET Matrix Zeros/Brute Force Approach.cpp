b9bc4afa07d8659ea48868a1e9750c545b1cc2c9
Raghav Bang <raghavbang2012@gmail.com>
2024-01-08 21:32:38 +0530

    Create Brute Force Approach.cpp

---
+++ A2Z/SET Matrix Zeros/Brute Force Approach.cpp
@@ -0,0 +1,31 @@
+https://leetcode.com/problems/set-matrix-zeroes/description/
+
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+
+vector<int> row;
+vector<int> column;
+
+for(int i=0;i<matrix.size();i++){
+    for(int j=0;j<matrix[0].size();j++){
+        if(matrix[i][j]==0){
+            row.push_back(i);
+            column.push_back(j);
+        }
+    }
+}
+
+for(int i=0;i<row.size();i++){
+    for(int j=0;j<matrix[0].size();j++){
+matrix[row[i]][j]=0;
+    }
+}
+for(int i=0;i<column.size();i++){
+    for(int j=0;j<matrix.size();j++){
+matrix[j][column[i]]=0;
+    }
+}
+        
+    }
+};
